4163a1560910aa2b41d852a78476a65759bce52e
thakaj <taulant.hakaj@scranton.edu>
2021-07-05 15:17:25 -0400

    added routes to index.js and my app.js so i can go from page to page of my project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Router, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import NewBandPage from './components/NewBandPage';
@@ -10,6 +10,17 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+        <Switch>
+          <Route path="/bandpage">
+              <BandPage/>
+          </Route>
+          <Route path="/newbandpage">
+            <NewBandPage/>
+          </Route>
+          <Route exact path="/homepage">
+              <HomePage/>          
+          </Route>
+        </Switch>
     </div>
   );
 }
